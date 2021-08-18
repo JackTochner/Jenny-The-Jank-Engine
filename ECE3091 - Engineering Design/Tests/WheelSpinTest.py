5290874010ae95cd512e26e4bf084283012a81a3
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-08-18 17:40:23 +1000

    Going forwards

--- ECE3091 - Engineering Design/Tests/WheelSpinTest.py
+++ ECE3091 - Engineering Design/Tests/WheelSpinTest.py
@@ -8,12 +8,12 @@ direction2 = gpiozero.OutputDevice(pin=27)
 encoder = gpiozero.RotaryEncoder(a=5, b=6,max_steps=100000) 
 # This class has a lot more functionality,so worth reading up on it
 # Step through duty cycle values, slowly increasing the speed and changing the direction of motion
+direction1.value = not direction1.value
+direction2.value = not direction2.value
 pre_steps = 0
 for j in range(10):
-    pwm1.value = 0.1
-    pwm2.value = 1
-    direction1.value = direction1.value
-    direction2.value = direction2.value
+    pwm1.value = 0.2
+    pwm2.value = 0.2
     print('Duty cycle:',pwm1.value,'Direction:',direction1.value)
     time.sleep(5.0)
     print('Counter:',encoder.steps,'Speed:',(encoder.steps-pre_steps)/5.0,'steps per second\n')
