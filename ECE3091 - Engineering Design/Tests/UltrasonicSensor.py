67e7a2201ac2f4fa07258a3518bdc6ec40223d25
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-08-23 13:37:25 +1000

    Update UltrasonicSensor.py

--- ECE3091 - Engineering Design/Tests/UltrasonicSensor.py
+++ ECE3091 - Engineering Design/Tests/UltrasonicSensor.py
@@ -17,11 +17,11 @@ import time
 #two front sensors - distractor detection
 # angle 15 degrees (need to detect distractors from afar?)
 
-sensorFront1 = gpiozero.DistanceSensor(echo=1,trigger=7) 
+#sensorFront1 = gpiozero.DistanceSensor(echo=1,trigger=7) 
 #echo connects to gpio 1 with 330 resistor and trigger to gpio 7 with 470 resistor
 #other end of both resistors goes to gpio (? any)
 
-sensorFront2 = gpiozero.DistanceSensor(echo=3,trigger=7) 
+#sensorFront2 = gpiozero.DistanceSensor(echo=3,trigger=7) 
 #echo connects to gpio 3 with 330 resistor and trigger to gpio 7 with 470 resistor
 #other end of both resistors goes to gpio (? any)
 
@@ -43,15 +43,15 @@ sensorLeft = gpiozero.DistanceSensor(echo=23,trigger=7)
 # changed to an infinite loop for j in range(10)
 while True:
     
-    distanceFront1 = sensorFront1.distance / 100 #mm to cm 
-    distanceFront2 = sensorFront2.distance / 100  
+    #distanceFront1 = sensorFront1.distance / 100 #mm to cm 
+    #distanceFront2 = sensorFront2.distance / 100  
     distanceRight = sensorRight.distance / 100 
     distanceLeft = sensorLeft.distance / 100 
     #maybe update to be in an array to save readings over time
 
     # print for testing
-    print('Distance of front 1: ', distanceFront1)
-    print('Distance of front 2: ', distanceFront2)
+    #print('Distance of front 1: ', distanceFront1)
+    #print('Distance of front 2: ', distanceFront2)
     print('Distance of right: ', distanceRight)
     print('Distance of left: ', distanceLeft)
     time.sleep(1)
