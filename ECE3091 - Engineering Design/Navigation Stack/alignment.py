44aa0092229211aae8036bd875375a76458a4d92
JackTochner <jtoc0001@student.monash.edu>
2021-08-26 12:44:18 +1000

    some cleanup

--- ECE3091 - Engineering Design/Navigation Stack/alignment.py
+++ ECE3091 - Engineering Design/Navigation Stack/alignment.py
@@ -68,56 +68,24 @@ def align():
 
 
 if __name__ == "__main__":
-    sensorFront1 = gpiozero.DistanceSensor(echo=23,trigger=5) 
-    #echo connects to gpio 1 with 330 resistor and trigger to gpio 7 with 470 resistor
-    #other end of both resistors goes to gpio (? any)
-
+    sensorFront1 = gpiozero.DistanceSensor(echo=23,trigger=5)     
     sensorFront2 = gpiozero.DistanceSensor(echo=24,trigger=6) 
-    #echo connects to gpio 1 with 330 resistor and trigger to gpio 7 with 470 resistor
-    #other end of both resistors goes to gpio (? any)
-
-
-    #left sensor and right sensor - wall alignment
-
-    #sensorRight = gpiozero.DistanceSensor(echo=24,trigger=7) 
-    #echo connects to gpio 1 with 330 resistor and trigger to gpio 7 with 470 resistor
-    #other end of both resistors goes to gpio (? any)
-
-    #sensorLeft = gpiozero.DistanceSensor(echo=23,trigger=7) 
-    #echo connects to gpio 1 with 330 resistor and trigger to gpio 7 with 470 resistor
-    #other end of both resistors goes to gpio (? any)
-
-    #define pwm values and output locations
+    
     pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
     pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 
-    # define the direction (this should be forward)
+    
     direction1 = gpiozero.OutputDevice(pin=4)
     direction2 = gpiozero.OutputDevice(pin=27)
 
-    #not sure if this works, but define which direction is forward
     forward = not direction1.value
 
-    #define encoder output
-    #encoder = gpiozero.RotaryEncoder(a=5, b=6,max_steps=100000) 
-
-    #error value means we may not be exactly aligned with the wall
     error = 0.1
 
-    print('Entering While Loop...')
-
-    distanceFront1 = sensorFront1.distance * 100 #mm to cm 
+    distanceFront1 = sensorFront1.distance * 100  
     distanceFront2 = sensorFront2.distance * 100  
-    #distanceRight = sensorRight.distance / 100 
-    #distanceLeft = sensorLeft.distance / 100 
-    #maybe update to be in an array to save readings over time
-
-    # print for testing
+    
     print('Distance of front 1: ', distanceFront1)
-    print('Distance of front 2: ', distanceFront2)
-    #print('Distance of right: ', distanceRight)
-    #print('Distance of left: ', distanceLeft)
-
-    #time.sleep(1)
+    print('Distance of front 2: ', distanceFront2)    
 
     align()
