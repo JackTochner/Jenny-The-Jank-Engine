1ed4f5a83a2c4bbffa80c734904c3ac488683918
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-08-27 15:04:47 +1000

    Update WheelSpinTest.py

--- ECE3091 - Engineering Design/Navigation Stack/Tests/WheelSpinTest.py
+++ ECE3091 - Engineering Design/Navigation Stack/Tests/WheelSpinTest.py
@@ -5,7 +5,6 @@ pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequenc
 pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 direction1 = gpiozero.OutputDevice(pin=4)
 direction2 = gpiozero.OutputDevice(pin=27)
-encoder = gpiozero.RotaryEncoder(a=5, b=6,max_steps=100000) 
 # This class has a lot more functionality,so worth reading up on it
 # Step through duty cycle values, slowly increasing the speed and changing the direction of motion
 direction1.value = not direction1.value
@@ -14,9 +13,7 @@ pre_steps = 0
 for j in range(2):
     pwm1.value = 1
     pwm2.value = 1
-    print('Duty cycle:',pwm1.value,'Direction:',direction1.value)
     time.sleep(5.0)
-    print('Counter:',encoder.steps,'Speed:',(encoder.steps-pre_steps)/5.0,'steps per second\n')
     pre_steps = encoder.steps
     
     #NB, if steps keeps increasing, what about integer overflows?
