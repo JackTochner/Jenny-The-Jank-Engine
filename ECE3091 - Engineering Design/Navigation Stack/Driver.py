508522e2b0a0f6c9ca056ea8ed9fe81ffe62c46b
JackTochner <jtoc0001@student.monash.edu>
2021-08-27 14:04:06 +1000

    updated files

--- ECE3091 - Engineering Design/Navigation Stack/Driver.py
+++ ECE3091 - Engineering Design/Navigation Stack/Driver.py
@@ -9,12 +9,17 @@ from Pin_Declaration import *
 
 print("Starting Alignment...")
 f.write("Starting Alignement...\n")
+
 align()
+
 print("Finished Alignement")
+f.write("Finsihed Alignement\n")
 
 print("Starting Obstacle Avoidance")
 f.write("Starting Obstacle Avoidance\n")
+
 obstacle_avoid()
+
 print("Finished Obstacle Avoidance")
 f.write("Finished Obstacle Avoidance\n")
 
