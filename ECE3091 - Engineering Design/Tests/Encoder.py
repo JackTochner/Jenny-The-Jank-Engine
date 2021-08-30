0c6a5c971e19417352661fed85f49b5d9789a5cc
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-08-30 14:54:56 +1000

    Update Encoder.py

--- ECE3091 - Engineering Design/Tests/Encoder.py
+++ ECE3091 - Engineering Design/Tests/Encoder.py
@@ -1,8 +1,8 @@
 import gpiozero
 import time
 
-rotary1 = gpiozero.RotaryEncoder(5,6)
-rotary2 = gpiozero.RotaryEncoder(23,24)
+rotary1 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
+rotary2 = gpiozero.RotaryEncoder(23,24, max_steps=100000)
 pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
 pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 direction1 = gpiozero.OutputDevice(pin=4)
