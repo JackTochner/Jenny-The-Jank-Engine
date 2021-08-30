124f3fe75ff957cfed1d0c5e5cd18cb6d0bd5f4d
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-08-30 15:30:38 +1000

    Update navigation.py

--- ECE3091 - Engineering Design/Navigation Stack/navigation.py
+++ ECE3091 - Engineering Design/Navigation Stack/navigation.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import numpy as np
-=======
+
 #import matplotlib
 import numpy as np
 #from matplotlib import pyplot as plt
->>>>>>> 885c2abea73abe9a066140b4abe9c6105acb950d
 #from IPython import display
 import time
 from Pin_Declaration import *
