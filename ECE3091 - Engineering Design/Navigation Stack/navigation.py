c078d788e8922ebffa6a28d8e4c28eb57214496f
JackTochner <jtoc0001@student.monash.edu>
2021-09-01 16:26:17 +1000

    Update navigation.py

--- ECE3091 - Engineering Design/Navigation Stack/navigation.py
+++ ECE3091 - Engineering Design/Navigation Stack/navigation.py
@@ -36,8 +36,9 @@ class DiffDriveRobot:
     def motor_simulator(self,w,duty_cycle):
          
         torque = self.I*duty_cycle
-        
+        print("torque: ", torque, "w: ", w)
         if (w > 0):
+            
             w = min(w + self.dt*(torque - self.d*w),3)
         elif (w < 0):
             w = max(w + self.dt*(torque - self.d*w),-3)
