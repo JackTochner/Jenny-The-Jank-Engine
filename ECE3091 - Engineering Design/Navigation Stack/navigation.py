bb997ad906a25a9db7c550c90af4d96372700882
Rill0002 <89002303+Rill0002@users.noreply.github.com>
2021-09-03 11:08:18 +1000

    Update navigation.py

--- ECE3091 - Engineering Design/Navigation Stack/navigation.py
+++ ECE3091 - Engineering Design/Navigation Stack/navigation.py
@@ -138,7 +138,7 @@ for i in range(100):
         pwm1.value,pwm2.value,direction1.value,direction2.value= controller.drive(0.1,0,robot.wl,robot.wr) #go straight for 20 secs
        
     elif i < 80: 
-         pwm1.value,pwm2.value,direction1.value,direction2.value= controller.drive(0.1,30,robot.wl,robot.wr) # turn in a full circle for 30 secs
+         pwm1.value,pwm2.value,direction1.value,direction2.value= controller.drive(0.1,20,robot.wl,robot.wr) # turn in a full circle for 30 secs
         
     # elif i > 150 or i < 200: # drive in circular path (turn right) for 10 s
     #     pwm1.value,pwm2.value,direction1.value,direction2.value = controller.drive(1,1,robot.wl,robot.wr)
