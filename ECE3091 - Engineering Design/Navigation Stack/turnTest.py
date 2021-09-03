f73fbaca3a33b6b3a4f721c8b96228cd2c407cb7
JackTochner <jtoc0001@student.monash.edu>
2021-09-03 13:01:41 +1000

    Create turnTest.py

---
+++ ECE3091 - Engineering Design/Navigation Stack/turnTest.py
@@ -0,0 +1,20 @@
+from Pin_Declaration import *
+
+straight1 = 37
+turn = straight1 + 30
+straight2 = turn + 30
+
+for i in range(200):
+    if i < straight1:
+        pwm1.value = 1
+        pwm2.value = 1
+
+    elif i < turn:
+        pwm2.value = 0
+
+    elif i < straight1:
+        pwm2.value = 1
+
+    else:
+        pwm1.value = 0
+        pwm2.value = 0
