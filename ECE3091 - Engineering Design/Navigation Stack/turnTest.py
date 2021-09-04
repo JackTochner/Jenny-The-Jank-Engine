893c45d417818dfe0766c627df5ef182361a764a
JackTochner <jtoc0001@student.monash.edu>
2021-09-04 10:20:54 +1000

    Update turnTest.py

--- ECE3091 - Engineering Design/Navigation Stack/turnTest.py
+++ ECE3091 - Engineering Design/Navigation Stack/turnTest.py
@@ -28,12 +28,14 @@ straight1 = 37
 for i in range(straight1):
     pwm1.value = 1
     pwm2.value = 1
+    time.sleep(0.1)
 
 turn(90)
 
 for i in range(straight1):
     pwm1.value = 1
     pwm2.value = 1
+    time.sleep(0.1)
 
 pwm1.value = 0
 pwm2.value = 0 
