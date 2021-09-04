6bec14fae6ebc66aacf0b37d57fb38c804bbb3b6
JackTochner <jtoc0001@student.monash.edu>
2021-09-04 11:00:38 +1000

    Update turnTest.py

--- ECE3091 - Engineering Design/Navigation Stack/turnTest.py
+++ ECE3091 - Engineering Design/Navigation Stack/turnTest.py
@@ -2,26 +2,28 @@ from Pin_Declaration import *
 from obstacle_avoidance import *
 import time
 
-straight1 = 640
+#straight1 = 640
 
-print("moving straight")
-for i in range(straight1):
-    
-    pwm1.value = 1
-    pwm2.value = 1
+straight1 = 37
 
-    
-    while obstacleCheck():
-        print("obstacle found! avoiding...")
-        avoid()
-    time.sleep(0.01)
-
-# turn(-90)
-
+# print("moving straight")
 # for i in range(straight1):
+    
 #     pwm1.value = 1
 #     pwm2.value = 1
-#     time.sleep(0.1)
 
-# pwm1.value = 0
-# pwm2.value = 0 
+    
+#     while obstacleCheck():
+#         print("obstacle found! avoiding...")
+#         avoid()
+#     time.sleep(0.01)
+
+turn(-90)
+
+for i in range(straight1):
+    pwm1.value = 1
+    pwm2.value = 1
+    time.sleep(0.1)
+
+pwm1.value = 0
+pwm2.value = 0 
