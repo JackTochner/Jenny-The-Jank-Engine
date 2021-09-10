84ef2a087f7e51dffe5e50b76e5320e0ca78bd48
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-10 14:45:33 +1000

    Update Fun.py

--- ECE3091 - Engineering Design/Tests/Fun.py
+++ ECE3091 - Engineering Design/Tests/Fun.py
@@ -1,1 +1,24 @@
 
+import gpiozero
+import datetime
+import os
+import time
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
+pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
+
+# define the direction (this should be forward)
+direction1 = gpiozero.OutputDevice(pin=4)
+direction2 = gpiozero.OutputDevice(pin=27)
+
+#not sure if this works, but define which direction is forward
+forward = direction1.value
+
+
+pwm1.value=1
+pwm2.value=1
