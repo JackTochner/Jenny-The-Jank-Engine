23f12ec401d9068cc32454abe3a69a84372316e7
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-10 17:05:32 +1000

    Update newNav2.py

--- ECE3091 - Engineering Design/Tests/newNav2.py
+++ ECE3091 - Engineering Design/Tests/newNav2.py
@@ -36,29 +36,46 @@ def motor_simulator():
   angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(stepsForFullTurn*0.01)
   angular2 = (2*math.pi*(rotary2.steps-pre_steps2))/(stepsForFullTurn*0.01)
     
-  return ((angular1+angular2)/2)
+  return angular1,angular2
   
   
-w = 0
-w_desired = 0.5
-w_measured = 0.0
-duty_cycle = 0
+class DiffDriveRobot:
+    
+    def __init__(self,inertia=5, dt=0.1, drag=0.2, wheel_radius=0.026, wheel_sep=0.15):
+        
+        self.x = 0.0 # y-position
+        self.y = 0.0 # y-position 
+        self.th = 0.0 # orientation
+        
+        self.wl = 0.0 #rotational velocity left wheel
+        self.wr = 0.0 #rotational velocity right wheel
+        
+        self.I = inertia
+        self.d = drag
+        self.dt = dt
+        
+        self.r = wheel_radius
+        self.l = wheel_sep
 
-e_sum = 0
-
-for j in range(1000):
+        
+    # Veclocity motion model
+    def base_velocity(self,wl,wr):
+        
+        v = (wl*self.r + wr*self.r)/2.0
+        
+        w = (wl - wr)/self.l
+        
+        return v, w
     
-    if(j>500):
-        w_desired=2.5
-    
-    pwm1.value=duty_cycle
-    pwm2.value=duty_cycle
-    
-    duty_cycle,e_sum = pwm_control(w_desired,w_measured,Kp=2.5,Ki=0.01,e_sum=e_sum)
-    
-    w_measured = motor_simulator()
-    
-    print('\n')
-    print(j, w_measured)
-    print(j, w_desired)
-    print(j, duty_cycle)
+    # Kinematic motion model
+    def pose_update(self,duty_cycle_l,duty_cycle_r):
+        
+        self.wr,self.wl = motor_simulator()
+        
+        v, w = self.base_velocity(self.wl,self.wr)
+        
+        self.x = self.x + self.dt*v*np.cos(self.th)
+        self.y = self.y + self.dt*v*np.sin(self.th)
+        self.th = self.th + w*self.dt
+        
+        return self.x, self.y, self.th
