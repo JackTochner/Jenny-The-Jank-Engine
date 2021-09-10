64abac796185522e85abbc21ed837d2c094d191b
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-10 15:01:34 +1000

    Update Encoder.py

--- ECE3091 - Engineering Design/Tests/Encoder.py
+++ ECE3091 - Engineering Design/Tests/Encoder.py
@@ -2,8 +2,8 @@ import gpiozero
 import time
 import math
 
-rotary1 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
-rotary2 = gpiozero.RotaryEncoder(23,24, max_steps=100000)
+rotary1 = gpiozero.RotaryEncoder(23,24, max_steps=100000)
+rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
 pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
 pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 direction1 = gpiozero.OutputDevice(pin=4)
@@ -12,7 +12,7 @@ direction2 = gpiozero.OutputDevice(pin=27)
 forward = not direction1.value
 
 pwm1.value = 1
-pwm2.value = 0
+pwm2.value = 1
 
 
 pre_steps1=0
@@ -22,7 +22,7 @@ maxSteps = 3650
 
 while True:
     
-    angular = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*5)
+    angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*5)
     
     string = 'Counter: ' + str(rotary1.steps) + '\tSpeed: ' + str((rotary1.steps-pre_steps1)/5) + 'steps per second' + '\tAngularVel: ' + str(angular)
 
@@ -31,6 +31,8 @@ while True:
     string = string + "\n"
 
 
+    angular2 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*5)
+    
     string = 'Counter: ' + str(rotary2.steps) + '\tSpeed: ' + str((rotary2.steps-pre_steps2)/5) + 'steps per second'
 
     print('rotary2:\n\n')
