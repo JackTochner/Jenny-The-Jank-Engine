8b3066f00b864fa4f855255b18c3dc1e75afc76f
JackTochner <jtoc0001@student.monash.edu>
2021-09-13 15:42:03 +1000

    output function should now work

--- ECE3091_Engineering_Design/Navigation_Stack/IgnoreThis.py
+++ ECE3091_Engineering_Design/Navigation_Stack/IgnoreThis.py
@@ -1,7 +1,26 @@
-#f = open("C:\\Users\\jackb\\OneDrive\\Documents\\GitHub\\Jenny-The-Jank-Engine\\ECE3091_Engineering_Design\\Navigation_Stack\\output.txt","w")
+import datetime
+import os
+
+now = datetime.datetime.now()
+current_time = now.strftime("%d-%m %H-%M-%S")
+file_name = "Jenny " + current_time 
+
+create_new_file = True
+
+if create_new_file:
+
+    file_name_text = file_name + ".txt"
+    nameOfFile = os.path.join("\\home\\pi\\Jenny-The-Jank-Engine\\ECE3091_Engineering_Design\\logs", file_name_text)
+    print(nameOfFile)
+    f = open(nameOfFile,"x")   
+
+#f = open("C:\\Users\\jackb\\OneDrive\\Documents\\GitHub\\Jenny-The-Jank-Engine\\ECE3091_Engineering_Design\\Navigation_Stack\\output2.txt","x")
+
 #/home/pi/Jenny-The-Jank-Engine/
 
-f = open("/home/pi/Jenny-The-Jank-Engine/ECE3091_Engineering_Design/Navigation_Stack/output.txt","w")
+
+
+#f = open("/home/pi/Jenny-The-Jank-Engine/ECE3091_Engineering_Design/Navigation_Stack/output2.txt","x")
 
 def output(string):
     print(string)
