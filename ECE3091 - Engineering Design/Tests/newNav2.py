e997d782a8f28df7f508215c3d8e81307da46919
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-13 13:41:27 +1000

    Update newNav2.py

--- ECE3091 - Engineering Design/Tests/newNav2.py
+++ ECE3091 - Engineering Design/Tests/newNav2.py
@@ -76,7 +76,7 @@ class DiffDriveRobot:
 
 class RobotController:
     
-    def __init__(self,Kp=2.5,Ki=0.01,wheel_radius=0.026, wheel_sep=0.13):
+    def __init__(self,Kp=1.0,Ki=0.001,wheel_radius=0.026, wheel_sep=0.13):
         
         self.Kp = Kp
         self.Ki = Ki
