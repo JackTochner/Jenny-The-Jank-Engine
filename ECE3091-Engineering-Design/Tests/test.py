8cdc7447cd2fe1083ddb8835df3f9a445342f380
JackTochner <jtoc0001@student.monash.edu>
2021-09-13 14:26:18 +1000

    Update test.py

--- ECE3091-Engineering-Design/Tests/test.py
+++ ECE3091-Engineering-Design/Tests/test.py
@@ -2,7 +2,8 @@ import sys
 
 sys.path.insert(0, 'Jenny-The-Jank-Engine/ECE3091-Engineering-Design/Navigation-Stack/')
 
-from Pin_Declaration import *
+from Navigation_Stack import Pin_Declaration
 
+from Pin_Declaration import *
 
 output("Hello")
