d46cc34ff85e89a4be8ddd86c02eac9b5f9da654
JackTochner <jtoc0001@student.monash.edu>
2021-09-13 15:11:13 +1000

    Update test.py

--- ECE3091_Engineering_Design/Tests/test.py
+++ ECE3091_Engineering_Design/Tests/test.py
@@ -1,12 +1,13 @@
 import sys
 
-
+sys.path.insert("/home/pi/Jenny-The-Jank-Engine/")
 #sys.path.insert(0, 'c:\\Users\\jackb\\OneDrive\\Documents\\GitHub\\Jenny-The-Jank-Engine')
 print(sys.path)
 
 # from Navigation_Stack import Pin_Declaration
 
 # from Pin_Declaration import *
+#['/home/pi/Jenny-The-Jank-Engine/ECE3091_Engineering_Design/Tests', '/usr/lib/python37.zip', '/usr/lib/python3.7', '/usr/lib/python3.7/lib-dynload', '/usr/local/lib/python3.7/dist-packages', '/usr/lib/python3/dist-packages']
 
 #from Navigation_Stack.Pin_Declaration import *
 from ECE3091_Engineering_Design.Navigation_Stack.Pin_Declaration import *
