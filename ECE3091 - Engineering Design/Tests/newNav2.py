a50cf1a4063895c14db0cdb8e9a3cf0867f151ed
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-13 13:31:31 +1000

    Update newNav2.py

--- ECE3091 - Engineering Design/Tests/newNav2.py
+++ ECE3091 - Engineering Design/Tests/newNav2.py
@@ -105,8 +105,8 @@ class RobotController:
         print(w_desired)
         print(v_desired/self.r)
         print(self.r*w_desired/2)
-        wl_desired = v_desired/self.r + self.l*w_desired/2 
-        wr_desired = v_desired/self.r - self.l*w_desired/2
+        wl_desired = v_desired/self.r + self.r*w_desired/2 
+        wr_desired = v_desired/self.r - self.r*w_desired/2
         print('Desired Angulars: \n')
         print(wl_desired)
         print(wr_desired)
