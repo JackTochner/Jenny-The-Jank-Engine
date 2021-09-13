c7e73cd7eba91d7fa3920033b9d9e3bb4a553994
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-13 16:24:58 +1000

    Update servo.py

--- ECE3091_Engineering_Design/Gripper Mechanisms/servo.py
+++ ECE3091_Engineering_Design/Gripper Mechanisms/servo.py
@@ -6,5 +6,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(5) # Initialization
 time.sleep(1)
