d88d95715784ac6461ea179f734aaa34da46610d
JackTochner <jtoc0001@student.monash.edu>
2021-09-15 17:59:28 +1000

    Update test.py

--- ECE3091_Engineering_Design/Tests/test.py
+++ ECE3091_Engineering_Design/Tests/test.py
@@ -9,9 +9,9 @@ camera.hflip = "True"
 
 #camera.capture("testPic.jpg")
 
-camera.capture_continuous("testCont.jpg")
+#camera.capture_continuous("testCont.jpg")
 
-# camera.capture_sequence("[testSeq1.jpg,testSeq2.jpg,testSeq3.jpg,testSeq4.jpg,testSeq5.jpg]")
+camera.capture_sequence("[testSeq1.jpg,testSeq2.jpg,testSeq3.jpg,testSeq4.jpg,testSeq5.jpg]")
 
 # camera.record_sequence("testVid.jpg")
 
