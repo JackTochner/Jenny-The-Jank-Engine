5ad07fdc9cd797aaec7558298e766f0945413dce
JackTochner <jtoc0001@student.monash.edu>
2021-09-15 17:45:06 +1000

    Update test.py

--- ECE3091_Engineering_Design/Tests/test.py
+++ ECE3091_Engineering_Design/Tests/test.py
@@ -3,15 +3,19 @@ from time import sleep
 
 camera = PiCamera()
 
+camera.vflip = "True"
+
+camera.hflip = "True"
+
 camera.capture("testPic.jpg")
 
-# camera.capture_continuous("testCont")
+# camera.capture_continuous("testCont.jpg")
 
-# camera.capture_sequence("testSeq")
+# camera.capture_sequence("[testSeq1.jpg,testSeq2.jpg,testSeq3.jpg,testSeq4.jpg,testSeq5.jpg]")
 
-# camera.record_sequence("testVid")
+# camera.record_sequence("testVid.jpg")
 
-# camera.start_record("testVid2")
+# camera.start_record("testVid2.jpg")
 
 camera.start_preview()
 sleep(5)
