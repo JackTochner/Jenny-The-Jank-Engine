97a480dc59c5b6921cd799970896e3c85e0e3c04
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-16 16:53:56 +1000

    Update newNav2.py

--- ECE3091_Engineering_Design/Tests/newNav2.py
+++ ECE3091_Engineering_Design/Tests/newNav2.py
@@ -6,6 +6,11 @@ import numpy as np
 rotary1 = gpiozero.RotaryEncoder(23,24, max_steps=100000)
 rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
 
+pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
+pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
+
+direction1 = gpiozero.OutputDevice(pin=4)
+direction2 = gpiozero.OutputDevice(pin=27)
 forward = direction1.value
 
 direction1.value = forward
