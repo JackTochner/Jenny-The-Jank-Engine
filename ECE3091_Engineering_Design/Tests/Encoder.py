958d7911eaecdea8aacd24a9dd5776e760b48a5e
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-16 17:02:46 +1000

    Update Encoder.py

--- ECE3091_Engineering_Design/Tests/Encoder.py
+++ ECE3091_Engineering_Design/Tests/Encoder.py
@@ -20,12 +20,15 @@ pre_steps2=0
 
 maxSteps = 3650
 
+wheelRadius = 0.026
+
 while True:
     print(rotary1.steps)
     
     angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*5)
+    velocity1 = angular1*wheelRadius
     
-    string = 'Counter: ' + str(rotary1.steps) + '\tSpeed: ' + str((rotary1.steps-pre_steps1)/5) + 'steps per second' + '\tAngularVel: ' + str(angular1)
+    string = 'Counter: ' + str(rotary1.steps) + '\tSpeed: ' + str((rotary1.steps-pre_steps1)/5) + 'steps per second' + '\tAngularVel: ' + str(angular1) + 'Linear V: ' + str(velocity1)
 
     print("rotary1:\n\n")
     print(string)
@@ -33,12 +36,15 @@ while True:
 
 
     angular2 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*5)
+    velocity2 = angular2*wheelRadius
     
-    string = 'Counter: ' + str(rotary2.steps) + '\tSpeed: ' + str((rotary2.steps-pre_steps2)/5) + 'steps per second'+ '\tAngularVel: ' + str(angular2)
+    string = 'Counter: ' + str(rotary2.steps) + '\tSpeed: ' + str((rotary2.steps-pre_steps2)/5) + 'steps per second'+ '\tAngularVel: ' + str(angular2) + 'Linear V: ' + str(velocity1)
 
     print('rotary2:\n\n')
     print(string)
     string = string + "\n"
+    
+    print("Average velocity: " + str((velocity1+velocity2)/2) + "Distance Travelled" + str((velocity1+velocity2)/2*5.0)
     
     pre_steps1 = rotary1.steps
     pre_steps2 = rotary2.steps
