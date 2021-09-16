95ff090db6f6d06eedcf3385e6f09935189b577c
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-16 16:56:02 +1000

    Update newNav2.py

--- ECE3091_Engineering_Design/Tests/newNav2.py
+++ ECE3091_Engineering_Design/Tests/newNav2.py
@@ -2,15 +2,13 @@ import gpiozero
 import time
 import math
 import numpy as np
-
+import sys
 rotary1 = gpiozero.RotaryEncoder(23,24, max_steps=100000)
 rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
 
-pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
-pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
+sys.path.insert(0,"/home/pi/Jenny-The-Jank-Engine/")
 
-direction1 = gpiozero.OutputDevice(pin=4)
-direction2 = gpiozero.OutputDevice(pin=27)
+from ECE3091_Engineering_Design.Navigation_Stack.Pin_Declaration import *
 forward = direction1.value
 
 direction1.value = forward
