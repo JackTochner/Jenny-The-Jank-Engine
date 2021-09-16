a3e890a8a352a5ab62c0e5e5f1892fb773e492a6
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-16 17:45:24 +1000

    Update newNav2.py

--- ECE3091_Engineering_Design/Tests/newNav2.py
+++ ECE3091_Engineering_Design/Tests/newNav2.py
@@ -126,7 +126,7 @@ class TentaclePlanner:
         self.dt = dt
         self.steps = steps
         # Tentacles are possible trajectories to follow
-        self.tentacles = [(0,0.01),(0.1,0),(0,-0.01)]
+        self.tentacles = [(0,0.1),(0.1,0),(0,-0.1)]
         
         self.alpha = alpha
         self.beta = beta
