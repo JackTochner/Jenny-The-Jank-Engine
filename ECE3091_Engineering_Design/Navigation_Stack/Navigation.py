1c9f1a5a137bb227607ec4983bfcc5d848a0aa56
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-09-19 20:20:52 +1000

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -234,7 +234,7 @@ for i in range(1000):
     output("goal_th-th")
     output(goal_th-th)
 
-    if abs(goal_x-x)<0.05 and abs(goal_y-y)<0.05 and abs(goal_th-th)<0.1:
+    if abs(goal_x-x)<0.05
         break
 
     timeArray.append(i)
