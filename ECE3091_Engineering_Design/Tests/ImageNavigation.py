494a7945eed3f9edf12c318a55f1747a0ac120cb
JackTochner <jtoc0001@student.monash.edu>
2021-09-27 13:55:36 +1000

    Update ImageNavigation.py

--- ECE3091_Engineering_Design/Tests/ImageNavigation.py
+++ ECE3091_Engineering_Design/Tests/ImageNavigation.py
@@ -1,3 +1,34 @@
 import sys
-
+import math
 sys.path.insert(0,"/home/pi/Jenny-The-Jank-Engine/")
+from ECE3091_Engineering_Design.Navigation_Stack.Pin_Declaration import *
+
+
+class coordinate:
+    def __init__(self,x,y):
+        self.x = x
+        self.y=y
+
+#TODO make this centrepoint of ball bearing
+ballCentre = coordinate(-1,-1)
+
+# is this the real resolution?
+imageWidth =  1440
+imageHeight = 1080
+
+imageCentre = coordinate(math.floor(imageWidth/2),math.floor(imageHeight/2))
+
+imageError = 0.1
+
+while ballCentre.x > imageCentre.x +imageError or ballCentre.x + imageError < imageCentre.x :
+    if ballCentre.x > imageCentre.x +imageError:
+        turn(-10)
+
+    else:
+        turn(10)
+
+
+    ballCentre.x = -1 #new position based on NN
+    ballCentre.y = -1 #new position based on NN
+
+
