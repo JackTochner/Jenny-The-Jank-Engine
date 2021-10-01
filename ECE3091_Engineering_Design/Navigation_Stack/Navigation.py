7db7a0b44e942958d9184210a029c0bffb033aaa
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-10-01 16:11:56 +1000

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -68,7 +68,7 @@ class DiffDriveRobot:
         
         v = (wl*self.r + wr*self.r)/2.0
         
-        w = (wr*self.r - wl*self.r)/self.l
+        w = (wl - wr)/self.l
         
         print('W\n')
         output(w)
