a6fb9ba8a720336cc335b52ce08620f549dc4197
JackTochner <jtoc0001@student.monash.edu>
2021-10-04 13:45:58 +1100

    Update Driver.py

--- ECE3091_Engineering_Design/Navigation_Stack/Driver.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Driver.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import sys
 
 sys.path.insert(0,"/home/pi/Jenny-The-Jank-Engine/")
-from ECE3091_Engineering_Design.Navigation_Stack.Alignment import GPIO_ECHO_FRONT1
+#from ECE3091_Engineering_Design.Navigation_Stack.Alignment import GPIO_ECHO_FRONT1
 from Pin_Declaration import *
 from Mapping import *
 from Alignment import Align
@@ -50,4 +50,6 @@ def main(align = False, navigate = False, comp=False):
         time.sleep(0.2)
 
 f.close()
+
+
 
