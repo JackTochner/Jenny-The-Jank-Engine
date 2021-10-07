5a0cd1c5a5c7d2d3face277b9db2de9d95bb1930
JackTochner <jtoc0001@student.monash.edu>
2021-10-08 10:25:09 +1100

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -354,7 +354,7 @@ def Navigate(x,y,th):
 
         totalTime= stop-start
 
-        if totalTime < 0.04:
+        if totalTime <= 0.04:
             time.sleep(0.04-totalTime)
         else:
             print("TOTAL TIME GREATER THAN 0.04: ", totalTime)
