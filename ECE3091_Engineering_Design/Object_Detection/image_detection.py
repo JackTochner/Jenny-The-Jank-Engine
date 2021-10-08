45a8d1877982d8ae4e9b8c45e70f4e4f813ed795
Rill0002 <89002303+Rill0002@users.noreply.github.com>
2021-10-08 11:31:16 +1100

    added either opencv or picamera method

--- ECE3091_Engineering_Design/Object_Detection/image_detection.py
+++ ECE3091_Engineering_Design/Object_Detection/image_detection.py
@@ -2,19 +2,28 @@ from detecto import core, utils, visualize
 from detecto.visualize import show_labeled_image, plot_prediction_grid
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import cv2
 
 #load model
 model = core.Model.load('model_weights_v1.pth', ['Targets'])
+
+#Using piCamera grab image fram
 #initialize camera
 camera = PiCamera()
 rawCapture = PiRGBArray(camera)
-
 time.sleep(0.1)
-
 #grab image from camera
 camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
-#image = utils.read_image(im) #
+
+#Using opencv to grab image frame
+# cap = cv2.VideoCapture(0)
+# ret, image = cap.read() #ret is a boolean indicating whether the grabbing process was successful or not
+
+# if ret: 
+#     predictions = model.predict(image)
+
+
 #run image thru model and get predictions
 predictions = model.predict(image)
 labels, boxes, scores = predictions
