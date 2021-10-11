0fd938d2449f94f0ef95fa4ceb519bacff5e8b82
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-10-11 15:09:07 +1100

    Update Encoder.py

--- ECE3091_Engineering_Design/Tests/Encoder.py
+++ ECE3091_Engineering_Design/Tests/Encoder.py
@@ -32,7 +32,7 @@ for i in range(200):
     
     time.sleep(0.02)
     
-    angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*0.02)
+    angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(maxSteps*0.02)*1.08
     total1 += angular1
     velocity1 = angular1*wheelRadius
     
@@ -43,7 +43,7 @@ for i in range(200):
     string = string + "\n"
 
 
-    angular2 = (2*math.pi*(rotary2.steps-pre_steps2))/(maxSteps*0.02)
+    angular2 = (2*math.pi*(rotary2.steps-pre_steps2))/(maxSteps*0.02)*1.23
     total2 += angular2
     velocity2 = angular2*wheelRadius
     
