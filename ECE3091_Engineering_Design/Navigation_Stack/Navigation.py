3ec78b66398bbc8e8e274030c5775f6a0ef7c979
JackTochner <jtoc0001@student.monash.edu>
2021-10-13 10:15:31 +1100

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -213,15 +213,15 @@ class TentaclePlanner:
         #         return np.nan    
 
         
-        if (obstacleDetected[0]):
+        if (not obstacleDetected[0]):
             if (v!=0 or w> 0 ):              
                 return np.nan
 
-        elif (obstacleDetected[1]):
+        elif (not obstacleDetected[1]):
             if (v!=0 or w> 0):         
                 return np.nan
 
-        elif (obstacleDetected[2]):
+        elif (not obstacleDetected[2]):
             if (v!=0 or w> 0 ):         
                 return np.nan
 
