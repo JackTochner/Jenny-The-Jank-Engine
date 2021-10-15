32ef74f926b0f372eec1b928aac6178f1a373e19
JackTochner <jtoc0001@student.monash.edu>
2021-10-15 20:08:46 +1100

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -371,6 +371,9 @@ def Navigate(x,y,th,distances,obstacleDetected,navIsDone):
         # output(goal_th-th)
         #timeArray.append(i)
 
+        if thpos >= 2*math.pi or thpos < -2*math.pi:
+            thpos = 0
+
         if  abs(goal_th-thpos)< 0.1 and abs(goal_x-xpos) < 0.01 and abs(goal_y-ypos) < 0.05:
 
         #if abs(goal_th-th) < 0.1 and abs(goal_x-x) < 0.01 and abs(goal_y-y) < 0.05:
