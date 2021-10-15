a2b12b0d40b370ae3b59bf7c4ef8d72a4a657081
JackTochner <jtoc0001@student.monash.edu>
2021-10-15 19:22:32 +1100

    Update Navigation.py

--- ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Navigation.py
@@ -185,7 +185,7 @@ class RobotController:
 
 class TentaclePlanner:
     
-    def __init__(self,dt=0.025,steps=15,alpha=5,beta=0.1):
+    def __init__(self,dt=0.025,steps=15,alpha=5,beta=0):
         
         self.dt = dt
         self.steps = steps
