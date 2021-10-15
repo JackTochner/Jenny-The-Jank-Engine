9e54ad81ba4b54411a80f19bf455f255e84f64f8
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-10-15 19:10:27 +1100

    Update newNav.py

--- ECE3091_Engineering_Design/Tests/newNav.py
+++ ECE3091_Engineering_Design/Tests/newNav.py
@@ -189,7 +189,7 @@ class RobotController:
 
 class TentaclePlanner:
     
-    def __init__(self,dt=0.0214,steps=25,alpha=5,beta=0.1):
+    def __init__(self,dt=0.0214,steps=20,alpha=5,beta=0.05):
         
         self.dt = dt
         self.steps = steps
