a43b0f840ba6fc567c346cdce7257331ec736522
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-10-16 12:09:39 +1100

    Update newNav.py

--- ECE3091_Engineering_Design/Tests/newNav.py
+++ ECE3091_Engineering_Design/Tests/newNav.py
@@ -84,7 +84,7 @@ def motor_simulator():
     time.sleep(0.02)
     
     angular1 = (2*math.pi*(rotary1.steps-pre_steps1))/(stepsForFullTurn*0.02)*1
-    angular2 = (2*math.pi*(rotary2.steps-pre_steps2))/(stepsForFullTurn*0.02)*2
+    angular2 = (2*math.pi*(rotary2.steps-pre_steps2))/(stepsForFullTurn*0.02)*1.5
     return angular1,angular2
   
   
