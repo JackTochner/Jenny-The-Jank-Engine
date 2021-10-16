f47ed0bb2c2a38f7fd3c3e36fd278cf1a6d0e98d
HarryEdwardes <89002990+HarryEdwardes@users.noreply.github.com>
2021-10-16 16:22:04 +1100

    Update newNav.py

--- ECE3091_Engineering_Design/Tests/newNav.py
+++ ECE3091_Engineering_Design/Tests/newNav.py
@@ -189,7 +189,7 @@ class RobotController:
 
 class TentaclePlanner:
     
-    def __init__(self,dt=0.0214,steps=20,alpha=5,beta=0):
+    def __init__(self,dt=0.0214,steps=20,alpha=5,beta=0.5):
         
         self.dt = dt
         self.steps = steps
@@ -363,6 +363,6 @@ outputcsv(navigationCsv,xArray)
 outputcsv(navigationCsv,yArray)
 
 
-Navigate(0.5,0,-90*(math.pi/180))
+Navigate(0,-0.3,-90*(math.pi/180))
 
 
