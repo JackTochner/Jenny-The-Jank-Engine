dc17da460226c7c9f0fb8e0655a9cae6a903002c
JackTochner <jtoc0001@student.monash.edu>
2021-10-16 12:18:30 +1100

    Update Driver.py

--- ECE3091_Engineering_Design/Navigation_Stack/Driver.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Driver.py
@@ -84,7 +84,7 @@ def main(align = False, navigate = False, comp=True):
 
                 US = Process(target = distance, args = (distances,obstacleDetected))
 
-                nav = Process(target = Navigate, args = (0.3,0,0,distances,obstacleDetected,pwm1,pwm2))            
+                nav = Process(target = Navigate, args = (0.1,0,0,distances,obstacleDetected,pwm1,pwm2))            
 
                 US.start()
                 #test.start()
