846bd98c8a513502aecad9ff9f03f8d604af6df7
JackTochner <jtoc0001@student.monash.edu>
2021-10-17 14:37:06 +1100

    Update Driver.py

--- ECE3091_Engineering_Design/Navigation_Stack/Driver.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Driver.py
@@ -96,7 +96,7 @@ def main(align = False, navigate = False, comp=True, scuffed_comp = False):
                 US = Process(target = distance, args = (distances,obstacleDetected))
 
                 #nav = Process(target = Navigate, args = (0.6,0,0,distances,obstacleDetected,foundObject))  
-                nav = Process(target = Navigate, args = (0.6,-0.5,0)     )
+                nav = Process(target = Navigate, args = (0.6,0,0)     )
 
                 NN = Process(target= detect_image, args = (foundObject,))       
 
