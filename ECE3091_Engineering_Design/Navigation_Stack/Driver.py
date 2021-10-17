e72c640d19eed8fa128c1fb17142461cf04c0ed2
JackTochner <jtoc0001@student.monash.edu>
2021-10-17 11:39:46 +1100

    full search mode

--- ECE3091_Engineering_Design/Navigation_Stack/Driver.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Driver.py
@@ -113,6 +113,68 @@ def main(align = False, navigate = False, comp=True, scuffed_comp = False):
                 
                 print("nav has finished")
 
+                ########################################################################################
+                # Nav 2
+                ########################################################################################
+
+                #print("is nav alive? " , nav.is_alive()) 
+                print('starting nav2')
+                nav2 = Process(target = Navigate, args = (0,0.7,90,distances,obstacleDetected,foundObject))                  
+
+                nav2.start()
+
+                nav2.join()  
+
+                #    if navIsDone:
+                print("nav2 has finished")
+
+                time.sleep(2)
+
+                
+
+                #print("is nav2 alive? " , nav2.is_alive())  
+
+                ########################################################################################
+                # Nav 3
+                ########################################################################################
+
+                print('starting nav3')
+                nav3 =  Process(target = Navigate, args = (0,0.7,90,distances,obstacleDetected,foundObject))                    
+
+                nav3.start()
+                nav3.join()
+
+                #        if navIsDone:
+                print("nav3 has finished")
+
+                time.sleep(2)
+
+                
+
+                #print("is nav3 alive? " , nav2.is_alive()) 
+
+                ########################################################################################
+                # Nav 4
+                ########################################################################################
+
+                print('starting nav4')
+                nav4 = Process(target = Navigate, args = (0,0.7,90,distances,obstacleDetected,foundObject))
+
+                    
+
+                nav4.start()
+                nav4.join()
+
+                US.terminate()
+
+
+
+                
+
+                print("NAV HAS FINISHED")
+
+                output("Finished")
+
 
     if scuffed_comp:
 
