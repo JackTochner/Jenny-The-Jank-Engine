fa81f449e4d23c6428894cd9ca862c2e334f282a
JackTochner <jtoc0001@student.monash.edu>
2021-10-17 14:22:08 +1100

    Update newNav.py

--- ECE3091_Engineering_Design/Navigation_Stack/newNav.py
+++ ECE3091_Engineering_Design/Navigation_Stack/newNav.py
@@ -3,10 +3,10 @@ import time
 import math
 import numpy as np
 import sys
-rotary1 = gpiozero.RotaryEncoder(24,23, max_steps=100000)
-rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
-pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
-pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
+# rotary1 = gpiozero.RotaryEncoder(24,23, max_steps=100000)
+# rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
+# pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
+# pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 
 sys.path.insert(0,"/home/pi/Jenny-The-Jank-Engine/")
 
@@ -281,6 +281,11 @@ duty_cycle_commands = []
 
 
 def Navigate(x,y,th):
+
+    rotary1 = gpiozero.RotaryEncoder(24,23, max_steps=100000)
+    rotary2 = gpiozero.RotaryEncoder(5,6, max_steps=100000)
+    pwm1 = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=50000)
+    pwm2 = gpiozero.PWMOutputDevice(pin=13,active_high=True,initial_value=0,frequency=50000)
 
     goal_x = x
     goal_y = y
