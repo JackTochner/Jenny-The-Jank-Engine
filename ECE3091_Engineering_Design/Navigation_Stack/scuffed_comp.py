d845d19f6c555cabb10b5de496696f20b6f71292
JackTochner <jtoc0001@student.monash.edu>
2021-10-17 17:48:30 +1100

    Update scuffed_comp.py

--- ECE3091_Engineering_Design/Navigation_Stack/scuffed_comp.py
+++ ECE3091_Engineering_Design/Navigation_Stack/scuffed_comp.py
@@ -107,6 +107,7 @@ def scuffed_comp_func(foundObject):
             driveToBall(foundObject[1],foundObject[2],foundObject[3],foundObject[4])
         
         if( i > 100 and i <107 )or (i > 200 and i <207) or (i > 300 and i < 307) or (i>400 and i <407):
+            print("drift correction")
             direction1.value = not forward
 
         else:
@@ -140,6 +141,7 @@ def scuffed_comp_func(foundObject):
 
 
         if( i > 100 and i <107 )or (i > 200 and i <207) or (i > 300 and i < 307) or (i>400 and i <407):
+            print("drift correction")
             direction1.value = not forward
 
         else:
