11e6149aa0c7afe3d0587bfecab6e37e8a40d3d0
JackTochner <jtoc0001@student.monash.edu>
2021-10-18 11:28:13 +1100

    Update scuffed_comp.py

--- ECE3091_Engineering_Design/Navigation_Stack/scuffed_comp.py
+++ ECE3091_Engineering_Design/Navigation_Stack/scuffed_comp.py
@@ -72,15 +72,17 @@ def scuffed_comp_func(foundObject):
 
         i = 0
         while True:
-            if( i > 100 and i <105 ):
+            # if( i > 100 and i <105 ):
+
                 
-                direction1.value = not forward
+                
+            #     direction1.value = not forward
 
-            else:
-                direction1.value = forward
+            # else:
+            #     direction1.value = forward
 
-            if i > 105:
-                i = 0
+            # if i > 105:
+            #     i = 0
 
 
             i += 1
