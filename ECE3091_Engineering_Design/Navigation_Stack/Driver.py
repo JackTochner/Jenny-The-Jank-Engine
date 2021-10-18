b9a1c1c3e228f255a318fad1466be04f9257bdf1
JackTochner <jtoc0001@student.monash.edu>
2021-10-18 12:03:25 +1100

    Update Driver.py

--- ECE3091_Engineering_Design/Navigation_Stack/Driver.py
+++ ECE3091_Engineering_Design/Navigation_Stack/Driver.py
@@ -212,7 +212,7 @@ def main(align = False, navigate = False, comp=False, scuffed_comp_bool = True):
             #test.start()
             nav.start()
 
-            time.sleep(1)
+            #time.sleep(1)
 
             NN.start() 
 
